7111e8f57f849303fdc1242cc3f842e303b4417e
Kiera Allen <kieraaallen@gmail.com>
2020-05-12 08:31:31 -0600

    Add and test name and length attributes

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -11,6 +11,11 @@ class ShipTest < Minitest::Test
   def test_it_exists
     assert_instance_of Ship, @cruiser
   end
+
+  def test_it_has_name_and_length
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+  end
 end
 
 
