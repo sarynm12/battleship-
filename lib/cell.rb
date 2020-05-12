257f1eef3443b6c4fab7fb985935577e1d2c5ae6
Kiera Allen <kieraaallen@gmail.com>
2020-05-12 11:42:22 -0600

    Add and test existance test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,1 +1,6 @@
+class Cell
 
+  def initialize(coordinate)
+    @coordinate = coordinate
+  end
+end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,1 +1,38 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
 
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+end
+
+
+
+
+# pry(main)> cell.coordinate
+# # => "B4"
+#
+# pry(main)> cell.ship
+# # => nil
+#
+# pry(main)> cell.empty?
+# # => true
+#
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# # => #<Ship:0x00007f84f0891238...>
+#
+# pry(main)> cell.place_ship(cruiser)
+#
+# pry(main)> cell.ship
+# # => #<Ship:0x00007f84f0891238...>
+#
+# pry(main)> cell.empty?
+# # => false
