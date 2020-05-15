e428116b67e061349c6cd7f9f4906687e135afe0
Kiera Allen <kieraaallen@gmail.com>
2020-05-14 20:19:18 -0600

    Create Board class and test files and test initialize method

---
+++ test/board_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_that_cells_method_returns_hash
+
+    # we can assert what we do know about this hash.
+    # it should have 16 key/value pairs, and those keys point to cell objects.
+
+    assert_instance_of Hash, @board.cells
+    # assert_equal 16, @board.cells.count
+    # assert_equal Cell, @cells["A1"]
+  end
+end
