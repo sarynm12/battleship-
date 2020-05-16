638acc4f5402b03c905295e88e15beea208c5e5c
Saryn Mooney <smooney15@my.whitworth.edu>
2020-05-16 12:12:18 -0600

    Sorry

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,4 @@
+
 class Cell
   attr_reader :coordinate,
               :ship
@@ -5,18 +6,39 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
-    if ship == nil
-      true
-    else
-      false
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+    if empty? == false
+      @ship.hit
     end
   end
 
-
-
-
-
+  def render(optional = false)
+    if empty? == false && @fired_upon == false && optional == true
+      "S"
+    elsif empty? == true && @fired_upon == true
+      "M"
+    elsif empty? == false && @ship.sunk? == true
+      "X"
+    elsif empty? == false && @fired_upon == true
+      "H"
+    else
+      "."
+    end
+  end
 end
